a5f635c0dfb800a12c165715e12104b55d2a1f3b
vishwakarmanikhil <menikhilv143@gmail.com>
2021-01-12 20:45:48 +0530

    remove all credential of amaze basket

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,7 +75,7 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><img src="assets/img/icons/menu.svg" class="m-icon"></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
